a7d1ca9d28592d2e9cd3f8f4d8d39fdd941b0c8b
dingpingchuan1 <dingpingchuan1@jd.com>
2020-10-29 09:14:31 +0800

    update word ladder

--- src/main/java/pers/captain/algorithm/October/WordLadder.java
+++ src/main/java/pers/captain/algorithm/October/WordLadder.java
@@ -1,10 +1,21 @@
 package pers.captain.algorithm.October;
 
-import java.util.List;
+import org.junit.Test;
+
+import java.util.*;
 
 /**
  * 127.单词接龙
  * 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+=======
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * 127. 单词接龙
+ * 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+>>>>>>> Stashed changes
  * <p>
  * 每次转换只能改变一个字母。
  * 转换过程中的中间单词必须是字典中的单词。
@@ -15,7 +26,11 @@ import java.util.List;
  * 所有单词只由小写字母组成。
  * 字典中不存在重复的单词。
  * 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+<<<<<<< Updated upstream
  * 示例 1:
+=======
+ * 示例 1:
+>>>>>>> Stashed changes
  * <p>
  * 输入:
  * beginWord = "hit",
@@ -33,6 +48,7 @@ import java.util.List;
  * endWord = "cog"
  * wordList = ["hot","dot","dog","lot","log"]
  * <p>
+<<<<<<< Updated upstream
  * 输出: 0
  * <p>
  * 解释: endWord "cog" 不在字典中，所以无法进行转换。
@@ -42,9 +58,87 @@ import java.util.List;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class WordLadder {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (beginWord == null || endWord == null || wordList == null) return 0;
 
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // 第 1 步：先将 wordList 放到哈希表里，便于判断某个单词是否在 wordList 里
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+            return 0;
+        }
+        wordSet.remove(beginWord);
+
+        // 第 2 步：图的广度优先遍历，必须使用队列和表示是否访问过的 visited 哈希表
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+
+        // 第 3 步：开始广度优先遍历，包含起点，因此初始化的时候步数为 1
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int currentSize = queue.size();
+            for (int i = 0; i < currentSize; i++) {
+                // 依次遍历当前队列中的单词
+                String currentWord = queue.poll();
+                // 如果 currentWord 能够修改 1 个字符与 endWord 相同，则返回 step + 1
+                if (changeWordEveryOneLetter(currentWord, endWord, queue, visited, wordSet)) {
+                    return step + 1;
+                }
+            }
+            step++;
+        }
         return 0;
     }
+
+    /**
+     * 尝试对 currentWord 修改每一个字符，看看是不是能与 endWord 匹配
+     *
+     * @param currentWord
+     * @param endWord
+     * @param queue
+     * @param visited
+     * @param wordSet
+     * @return
+     */
+    private boolean changeWordEveryOneLetter(String currentWord, String endWord,
+                                             Queue<String> queue, Set<String> visited, Set<String> wordSet) {
+        char[] charArray = currentWord.toCharArray();
+        for (int i = 0; i < endWord.length(); i++) {
+            // 先保存，然后恢复
+            char originChar = charArray[i];
+            for (char k = 'a'; k <= 'z'; k++) {
+                if (k == originChar) {
+                    continue;
+                }
+                charArray[i] = k;
+                String nextWord = String.valueOf(charArray);
+                if (wordSet.contains(nextWord)) {
+                    if (nextWord.equals(endWord)) {
+                        return true;
+                    }
+                    if (!visited.contains(nextWord)) {
+                        queue.add(nextWord);
+                        // 注意：添加到队列以后，必须马上标记为已经访问
+                        visited.add(nextWord);
+                    }
+                }
+            }
+            // 恢复
+            charArray[i] = originChar;
+        }
+        return false;
+    }
+
+    @Test
+    public void test() {
+        String[] words = new String[]{"hot", "dot", "dog", "lot", "log", "cog"};
+        List wordList = new ArrayList();
+        for (String word : words) {
+            wordList.add(word);
+        }
+        ladderLength("hit", "cog", wordList);
+    }
+
+
 }
