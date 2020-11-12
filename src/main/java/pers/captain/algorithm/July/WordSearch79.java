e92dce40505f0e68d1957940610bbbb5ece2fa1e
Captain <951289399@qq.com>
2020-11-13 07:11:23 +0800

    update word search

--- src/main/java/pers/captain/algorithm/July/WordSearch79.java
+++ src/main/java/pers/captain/algorithm/July/WordSearch79.java
@@ -27,38 +27,77 @@ package pers.captain.algorithm.July;
 public class WordSearch79 {
 
 
-    public boolean exist(char[][] board, String word) {
-        if (board == null || board.length == 0 || board[0].length == 0
-                || word == null || word.length() == 0) return false;
-        boolean[][] road = new boolean[board.length][board[0].length];
-
-        int startRow = 0;
-        int startCol = 0;
-        boolean flag = false;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (word.charAt(0) == board[i][j]) {
-                    startRow = i;
-                    startCol = j;
-                    flag = true;
-                    road[i][j] = true;
-                }
-            }
-        }
-        if (flag) {
-            return helper(board, road, startRow, startCol, word.substring(1));
-        }
-        return false;
-    }
-
-    private boolean helper(char[][] board, boolean[][] road, int row, int col, String restWord) {
-
-        return true;
-    }
-
     public static void main(String[] args) {
         String str = "2345";
         System.out.println(str.substring(1));
     }
 
+
+    /**
+     * @param board
+     * @param word
+     * @return
+     */
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0 || word == null) return false;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    // 从上到下，以每个点为开始，想四个方向找。
+                    boolean[][] isUsed = new boolean[board.length][board[0].length];
+                    boolean result = helper(board, word, i, j, 0, isUsed);
+                    if (result) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean helper(char[][] board, String word, int row, int col, int wordIndex, boolean[][] isUsed) {
+        if (wordIndex == word.length()) return true;
+        // 已经使用，标识之前走过
+        if (row >= board.length || row < 0 || col >= board[0].length || col < 0 || isUsed[row][col]) return false;
+        if (board[row][col] == word.charAt(wordIndex)) {
+            isUsed[row][col] = true;
+            wordIndex++;
+            if (helper(board, word, row - 1, col, wordIndex, isUsed)) return true;// 上
+            if (helper(board, word, row + 1, col, wordIndex, isUsed)) return true;// 下
+            if (helper(board, word, row, col - 1, wordIndex, isUsed)) return true; // 左
+            if (helper(board, word, row, col + 1, wordIndex, isUsed)) return true; // 右
+            isUsed[row][col] = false;
+        }
+        return false;
+    }
+
+    //使用数字本身来标记是否已经走过该位置，  但是如果能找到这个单词，原来的二维数字是被改变了（找到的路径变成了'.'）
+    public boolean exist_S(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0 || word == null) return false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    // 从上到下，以每个点为开始，想四个方向找。
+                    boolean result = helper(board, word, i, j, 0);
+                    if (result) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean helper(char[][] board, String word, int row, int col, int wordIndex) {
+        if (wordIndex == word.length()) return true;
+        // 已经使用，标识之前走过
+        if (row >= board.length || row < 0 || col >= board[0].length || col < 0 || board[row][col] == '.') return false;
+        if (board[row][col] == word.charAt(wordIndex)) {
+            board[row][col] = '.';//标记已经走过
+            wordIndex++;
+            if (helper(board, word, row - 1, col, wordIndex)) return true;// 上
+            if (helper(board, word, row + 1, col, wordIndex)) return true;// 下
+            if (helper(board, word, row, col - 1, wordIndex)) return true; // 左
+            if (helper(board, word, row, col + 1, wordIndex)) return true; // 右
+            board[row][col] = word.charAt(wordIndex - 1);// 还原
+        }
+        return false;
+    }
 }
--- src/main/java/pers/captain/algorithm/nov/CoinChange.java
+++ src/main/java/pers/captain/algorithm/nov/CoinChange.java
@@ -37,29 +37,61 @@ import java.util.Arrays;
  */
 public class CoinChange {
 
-    public int coinChange(int[] coins, int amount) {
+    /**
+     * 回溯的思路不对！ 回溯的思路不对
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange_error(int[] coins, int amount) {
         if (amount == 0 || coins == null) return -1;
-        int[] count = new int[1];
         Arrays.sort(coins);
-        helper(coins, amount, 0, count);
-        return count[0] == 0 ? -1 : count[0];
-
+        return helper(coins, amount, coins.length - 1, 1);
     }
 
-    private void helper(int[] coins, int amount, int curIndex, int[] count) {
-        for (int i = curIndex; i < coins.length; i++) {
+    private int helper(int[] coins, int amount, int curIndex, int depth) {
+        int count = -1;
+        for (int i = curIndex; i >= 0; i--) {
+            if (amount == coins[i])
+                return depth;
             if (amount > coins[i]) {
-                helper(coins, amount - coins[i], i, count);
-            } else if (amount == coins[i]) {
-                count[0]++;
+                count = helper(coins, amount - coins[i], i, depth + 1);
+                if (count != -1) break;
             }
         }
+        return count;
     }
+
+    /**
+     * 贪心算法
+     * 1. 优先使用大货币，计算需要的数量  amount/ 币  想下取整
+     * 2. 剩余的数如果为零 结果，若果剩余的数，小于最小的货币，结束，返回-1 。不为零用另一个小的货币执行步骤1
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        if (coins == null) return -1;
+        Arrays.sort(coins);
+        int count = 0;
+        for (int i = coins.length - 1; i >= 0; i--) {
+            int n = amount / coins[i];
+            count += n;
+            amount = amount % coins[i];
+            // 剩余的数量为零结束，找到最少货币组合
+            if (amount == 0) break;
+        }
+        return amount==0? count: -1;
+    }
+
 
     @Test
     public void coinChange() {
-        int[] coins = new int[]{1, 2, 5};
-        int count = coinChange(coins, 11);
+        int[] coins = new int[]{186, 419, 83, 408};
+        int count = coinChange(coins, 6249);
         CapL.print(count);
     }
 
