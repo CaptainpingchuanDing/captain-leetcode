1036121fa168531254cfe49b245467e0f04beddc
dingpingchuan1 <dingpingchuan1@jd.com>
2021-03-06 11:20:49 +0800

    add offer 55 63  todo 46

--- src/main/java/pers/captain/algorithm/offer/six/NO63GetMedian.java
+++ src/main/java/pers/captain/algorithm/offer/six/NO63GetMedian.java
@@ -1,19 +1,42 @@
 package pers.captain.algorithm.offer.six;
 
-/**
- * 题目描述
- * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。
- * 如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
- * 我们使用Insert()方法读取数据流，使用GetMedian()方法获取当前读取数据的中位数。
- */
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
 public class NO63GetMedian {
 
-    public void Insert(Integer num) {
+    private PriorityQueue<Integer> min = new PriorityQueue<>();
+    private PriorityQueue<Integer> max = new PriorityQueue<>((o1, o2) -> o2 - o1);
+    private int count;
 
+    public void Insert(Integer num) {
+        if (count % 2 == 0) {
+            max.offer(num);
+            int temp = max.poll();
+            min.offer(temp);
+        } else {
+            min.offer(num);
+            int temp = min.poll();
+            max.offer(temp);
+        }
+        count++;
     }
 
     public Double GetMedian() {
+        if (count % 2 == 0) {
+            return (min.peek() + max.peek()) / 2.0;
+        } else {
+            return min.peek() / 1.0;
+        }
+    }
 
-        return 0.0;
+    @Test
+    public void test(){
+        Insert(3);
+        Insert(4);
+        double res = GetMedian();
+        Assert.assertEquals(3.5,res,0.1);
     }
 }
